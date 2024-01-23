916891dc8b21df1fd17c8a9b2d0b7a86e0b9665e
KayT <kkt@ct-planet.co.kr>
2024-01-23 11:26:12 +0900

    feat:api

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+error_reporting(1);
+require_once $_SERVER["DOCUMENT_ROOT"] . '/app/service/Pwe.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/app/service/CtplanetService.php';
+
+use app\service\Pwe;
+use app\service\CtplanetService;
+
+$_actionMode = isset($_POST['mode']) ? $_POST['mode'] : null;
+
+$pwe = new Pwe();
+
+switch ($_actionMode) {
+
+  case "getList":
+    try {
+      $ctplanetService = new CtplanetService("portfolio");
+      $team = $_POST['team'];
+
+      echo json_encode($ctplanetService->referenceList($team));
+
+    } catch (Exception $e) {
+      // var_dump($e->getMessage());
+      echo json_encode([
+        "result" => 400
+      ]);
+    }
+    break;
+
+  case "getDetail":
+    try {
+      $ctplanetService = new CtplanetService("portfolio");
+      $seq = isset($_POST['seq']) ? $_POST['seq'] : null;
+
+      echo json_encode($ctplanetService->referenceDetail($seq));
+
+    } catch (Exception $e) {
+      // var_dump($e->getMessage());
+      echo json_encode([
+        "result" => 400
+      ]);
+    }
+    break;
+}
+exit;
